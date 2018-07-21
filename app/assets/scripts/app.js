4b4ef84b5787fd1e065d70f53e7fb1109c291189
Artemy <artie.hayden@gmail.com>
2018-07-21 19:05:55 +0300

    Complited fixed header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScrol from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 
 new RevealOnScrol($(".feature-item"), "85%");
 new RevealOnScrol($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
